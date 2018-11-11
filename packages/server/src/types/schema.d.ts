1d5a83f89e6fe03a42813d49542ba2a3e3ac93ac
Matthew Hoth <matthewhoth@gmail.com>
2018-11-11 16:37:17 +0800

    Dependencies update. Whoop whoop

--- packages/server/src/types/schema.d.ts
+++ packages/server/src/types/schema.d.ts
@@ -1,62 +1,62 @@
 // tslint:disable
 // graphql typescript definitions
 
-export namespace GQL {
-  interface IGraphQLResponseRoot {
-    data?: IQuery | IMutation;
-    errors?: Array<IGraphQLResponseError>;
-  }
+declare namespace GQL {
+interface IGraphQLResponseRoot {
+data?: IQuery | IMutation;
+errors?: Array<IGraphQLResponseError>;
+}
 
-  interface IGraphQLResponseError {
-    /** Required for all errors */
-    message: string;
-    locations?: Array<IGraphQLResponseErrorLocation>;
-    /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-    [propName: string]: any;
-  }
+interface IGraphQLResponseError {
+/** Required for all errors */
+message: string;
+locations?: Array<IGraphQLResponseErrorLocation>;
+/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+[propName: string]: any;
+}
 
-  interface IGraphQLResponseErrorLocation {
-    line: number;
-    column: number;
-  }
+interface IGraphQLResponseErrorLocation {
+line: number;
+column: number;
+}
 
-  interface IQuery {
-    __typename: "Query";
-    me: IUser | null;
-  }
+interface IQuery {
+__typename: "Query";
+me: IUser | null;
+}
 
-  interface IUser {
-    __typename: "User";
-    _id: string;
-    email: string;
-  }
+interface IUser {
+__typename: "User";
+_id: string;
+email: string;
+}
 
-  interface IMutation {
-    __typename: "Mutation";
-    login: ILoginResponse;
-    register: Array<IError> | null;
-  }
+interface IMutation {
+__typename: "Mutation";
+login: ILoginResponse;
+register: Array<IError> | null;
+}
 
-  interface ILoginOnMutationArguments {
-    email: string;
-    password: string;
-  }
+interface ILoginOnMutationArguments {
+email: string;
+password: string;
+}
 
-  interface IRegisterOnMutationArguments {
-    email: string;
-    password: string;
-  }
+interface IRegisterOnMutationArguments {
+email: string;
+password: string;
+}
 
-  interface ILoginResponse {
-    __typename: "LoginResponse";
-    errors: Array<IError> | null;
-  }
+interface ILoginResponse {
+__typename: "LoginResponse";
+errors: Array<IError> | null;
+}
 
-  interface IError {
-    __typename: "Error";
-    path: string;
-    message: string;
-  }
+interface IError {
+__typename: "Error";
+path: string;
+message: string;
+}
 }
 
 // tslint:enable
