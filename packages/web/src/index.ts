65aa5831beafbb8e3e243fe2c231f27f24a71376
Matthew Hoth <matthewhoth@gmail.com>
2018-11-12 14:27:07 +0800

    Run express on front end start up

---
+++ packages/web/src/index.ts
@@ -0,0 +1,14 @@
+import * as next from "next";
+import * as express from "express";
+import routes from "./routes/index";
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = routes.getRequestHandler(app);
+app.prepare().then(() => {
+  const server = express();
+  server.get("*", (req, res) => handle(req, res));
+  server.listen({ port }, () =>
+    console.log(`🚀 Client ready at http://localhost:${port}`)
+  );
+});
