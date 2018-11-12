23aa86577e39dfd536fca56f547dda37b37c8b10
Matthew Hoth <matthewhoth@gmail.com>
2018-11-12 13:32:00 +0800

    Create certs for a recipient and then email that recipient using sendgrind

--- packages/server/src/entity/Credentials.ts
+++ packages/server/src/entity/Credentials.ts
@@ -1,16 +1,16 @@
 import { Document, Schema, model } from "mongoose";
 export interface ICredentialModel extends Document {
   recipient: string;
-  email: string;
+  recipientsEmail: string;
   groupId: string;
   issueDate: Date;
   createdAt: Date;
 }
 const CredentialSchema = new Schema({
   recipient: { type: String },
-  email: { type: String },
+  recipientsEmail: { type: String },
   groupId: { type: String },
-  issueDate: { type: Date, default: Date.now },
+  issueDate: { type: Date },
   createdAt: { type: Date, default: Date.now }
 });
 export const Credential = model<ICredentialModel>(
---
+++ packages/server/src/modules/credential/publish/errorMessages.ts
@@ -0,0 +1,1 @@
+export const notExist = "this ___ does not exist";
